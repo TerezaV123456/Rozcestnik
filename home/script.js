485b2927c1b6a0f73b8f6fdd73211492f64f2b32
TerezaV <vavruskova@alpodgroup.cz>
2025-03-31 11:17:51 +0200

    update

--- home/script.js
+++ home/script.js
@@ -1,3 +1,22 @@
+function filterBoxes() {
+    let searchValue = document.getElementById("search").value.toLowerCase();
+    let filterValue = document.getElementById("filter").value;
+    let boxes = document.querySelectorAll(".box");
+    
+    boxes.forEach(box => {
+        let text = box.textContent.toLowerCase();
+        let tags = box.getAttribute("data-tags");
+        let matchSearch = text.includes(searchValue);
+        let matchFilter = (filterValue === "all" || tags.includes(filterValue));
+        
+        if (matchSearch && matchFilter) {
+            box.style.display = "flex";
+        } else {
+            box.style.display = "none";
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".box a:first-child").forEach(link => {
         const url = new URL(link.href);
